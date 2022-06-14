ffa0f753da6b371b94860a30043fe5c058bf9bc1
CedricMlg <malingre.cedric@gmail.com>
2022-06-14 17:24:42 +0200

    Photographes recup depuis le json,use module js

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,33 @@
-function photographerFactory(data) {
-  const { name, portrait } = data;
+export default function photographerFactory(data) {
+  const { portrait, name, city, country, tagline, price } = data;
 
   const picture = `assets/photographers/${portrait}`;
 
   function getUserCardDOM() {
     const article = document.createElement("article");
     const img = document.createElement("img");
-    img.setAttribute("src", picture);
     const h2 = document.createElement("h2");
+    const place = document.createElement("p");
+    const location = document.createElement("p");
+    const tag = document.createElement("p");
+    const money = document.createElement("p");
+
+    img.setAttribute("src", picture);
+
     h2.textContent = name;
+    place.textContent = city;
+    location.textContent = country;
+    tag.textContent = tagline;
+    money.textContent = `${price}€/jour`;
+
     article.appendChild(img);
     article.appendChild(h2);
+    article.appendChild(place);
+    article.appendChild(location);
+    article.appendChild(tag);
+    article.appendChild(money);
+
     return article;
   }
-  return { name, picture, getUserCardDOM };
+  return { name, picture, tagline, price, getUserCardDOM };
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,10 @@
+import photographerFactory from "../factories/photographer.js";
+import "./../../sass/main.scss";
+
 async function getPhotographers() {
-  // Penser à remplacer par les données récupérées dans le json
-  const photographers = [
-    {
-      // "name": "Ma data test",
-      // "id": 1,
-      // "city": "Paris",
-      // "country": "France",
-      // "tagline": "Ceci est ma data test",
-      // "price": 400,
-      // "portrait": "account.png"
-    //   readJson() {
-    //     fetch("../../data/photographers.json")
-    //       .then((response) => {
-    //         if (!response.ok) {
-    //           throw new Error("HTTP error " + response.status);
-    //         }
-    //         return response.json();
-    //       })
-    //       .then((data) => {
-    //         console.log(data);
-    //       })
-    //       .catch(function (error) {
-    //         console.log(
-    //           "Il y a eu un problème avec l'opération fetch: " + error.message
-    //         );
-    //       });
-    //   },
-    },
-    {
-      name: "Autre data test",
-      id: 2,
-      city: "Londres",
-      country: "UK",
-      tagline: "Ceci est ma data test 2",
-      price: 500,
-      portrait: "account.png",
-    },
-  ];
-  // et bien retourner le tableau photographers seulement une fois
-  return {
-    photographers: [...photographers, ...photographers, ...photographers],
-  };
+  const response = await fetch("../../data/photographers.json");
+  const data = await response.json();
+  return { photographers: data.photographers };
 }
 
 async function displayData(photographers) {
