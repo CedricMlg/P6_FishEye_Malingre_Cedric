88970c3ae9c25d69eb7a34f68b546d314ac3d638
CedricMlg <malingre.cedric@gmail.com>
2022-07-05 17:53:18 +0200

    Upgrade form avec champs requis, opti avec class

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,10 +1,84 @@
 import photographerFactory from "../factories/photographer.js";
 import { FactoryMedia } from "../factories/media.js";
-import "../utils/contactForm.js";
+import { Form } from "../utils/contactForm.js";
 import "./../../sass/main.scss";
+
+const submitBtn = document.getElementById("submit-form");
+const modal = document.getElementById("contact_modal");
+const modalOpen = document.querySelectorAll(".modal-open");
+const modalClose = document.querySelectorAll(".modal-close");
+let previousActiveElement;
+
+const KEYCODE = {
+  ESC: 27,
+};
 
 const paramsURL = new URL(document.location).searchParams;
 const idPhotographer = paramsURL.get("id");
+
+modalOpen.forEach((trigger) => trigger.addEventListener("click", openModal));
+
+modalClose.forEach((trigger) => trigger.addEventListener("click", closeModal));
+
+submitBtn.addEventListener("click", (envoi) => {
+  let prenom = document.getElementById("First-name").value;
+  let nom = document.getElementById("Last-name").value;
+  let email = document.getElementById("Email").value;
+  let message = document.getElementById("Your-message").value;
+  let form = new Form(prenom, nom, email, message);
+  form.checkPrenom();
+  form.checkNom();
+  form.checkEmail();
+  form.checkMessage();
+  form.checkValid();
+});
+
+/**
+ * When the modal is opened, the user is unable to scroll the page, and the modal is focused.
+ */
+function openModal() {
+  document.addEventListener("keydown", checkCloseModal);
+  previousActiveElement = document.activeElement;
+
+  Array.from(document.body.children).forEach((child) => {
+    if (child !== modal) {
+      child.inert = true;
+    }
+  });
+  modal.classList.add("active");
+  document.body.style.overflowY = "hidden";
+
+  modal.querySelector(".modal__close-button").focus();
+}
+
+/**
+ * If the key pressed is the escape key, then close the modal
+ * @param e - The event object
+ */
+function checkCloseModal(e) {
+  if (e.keyCode === KEYCODE.ESC) {
+    closeModal();
+  }
+}
+
+/**
+ * It removes the event listener that listens for the escape key, removes the inert attribute from all
+ * the elements in the body except the modal, removes the active class from the modal, and sets the
+ * overflowY property of the body to an empty string.
+ */
+function closeModal() {
+  document.removeEventListener("keydown", checkCloseModal);
+
+  Array.from(document.body.children).forEach((child) => {
+    if (child !== modal) {
+      child.inert = false;
+    }
+  });
+  modal.classList.remove("active");
+  document.body.style.overflowY = "";
+
+  previousActiveElement.focus();
+}
 
 /**
  * It fetches the data from the JSON file and returns it as an object.
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,71 +1,95 @@
-const modal = document.getElementById("contact_modal");
-const modalOpen = document.querySelectorAll(".modal-open");
-const modalClose = document.querySelectorAll(".modal-close");
-const submitBtn = document.getElementById("submit-form");
-let previousActiveElement;
+class Form {
+  constructor(prenom, nom, email, message) {
+    this.prenom = prenom;
+    this.formPrenom = document.getElementById("formFirst");
+    this.alertPrenom = document.getElementById("formFAlert");
+    this.nom = nom;
+    this.formNom = document.getElementById("formLast");
+    this.alertNom = document.getElementById("formLAlert");
+    this.email = email;
+    this.formMail = document.getElementById("formEmail");
+    this.alertEmail = document.getElementById("formEAlert");
+    this.message = message;
+    this.formMessage = document.getElementById("formMsg");
+    this.alertMessage = document.getElementById("formMAlert");
+    this.modalForm = document.querySelector("modal-form");
+    this.modalFormData = document.querySelectorAll("div.modal-form__data");
+  }
 
-const KEYCODE = {
-  ESC: 27,
-};
+  checkPrenom() {
+    let condition = this.prenom.length > 2;
+    let alert = this.alertPrenom;
+    let input = this.formPrenom;
+    let textError = "Veuillez renseigner un prenom de plus de deux caractères.";
 
-modalOpen.forEach((trigger) => trigger.addEventListener("click", openModal));
+    this.controlInput(condition, alert, input, textError);
+  }
 
-modalClose.forEach((trigger) => trigger.addEventListener("click", closeModal));
+  checkNom() {
+    let condition = this.nom.length > 2;
+    let alert = this.alertNom;
+    let input = this.formNom;
+    let textError = "Veuillez renseigner un nom de plus de deux caractères.";
 
-submitBtn.addEventListener("click", (envoi) => {
-  let prenom = document.getElementById("First-name").value;
-  let nom = document.getElementById("Last-name").value;
-  let email = document.getElementById("Email").value;
-  let message = document.getElementById("Your-message").value;
-  console.log(prenom, nom, email, message);
-});
+    this.controlInput(condition, alert, input, textError);
+  }
 
+  checkEmail() {
+    const mailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let condition = this.email != "" && mailReg.test(this.email);
+    let alert = this.alertEmail;
+    let input = this.formMail;
+    let textError = "Veuillez renseigner une adresse email valide.";
 
+    this.controlInput(condition, alert, input, textError);
+  }
 
-/**
- * When the modal is opened, the user is unable to scroll the page, and the modal is focused.
- */
-function openModal() {
-  document.addEventListener("keydown", checkCloseModal);
-  previousActiveElement = document.activeElement;
+  checkMessage() {
+    let condition = this.message != "" && this.message.length > 30;
+    let alert = this.alertMessage;
+    let input = this.formMessage;
+    let textError = "Veuillez écrire un message d'au moins 30 caractères.";
 
-  Array.from(document.body.children).forEach((child) => {
-    if (child !== modal) {
-      child.inert = true;
+    this.controlInput(condition, alert, input, textError);
+  }
+
+  /**
+   * If the condition is false, then set the alert's innerHTML to the txtError, and set the input's
+   * data-error-visible attribute to true. Otherwise, set the input's data-error-visible attribute to
+   * false, and set the alert's innerHTML to an empty string.
+   * @param condition - the condition that will be checked
+   * @param alert - the element that will display the error message
+   * @param input - the input element
+   * @param txtError - The text that will be displayed in the alert box.
+   */
+  controlInput(condition, alert, input, txtError) {
+    if (condition == false) {
+      alert.innerHTML = txtError;
+      input.setAttribute("data-error-visible", "true");
+    } else {
+      input.setAttribute("data-error-visible", "false");
+      alert.innerHTML = "";
     }
-  });
-  modal.classList.add("active");
-  document.body.style.overflowY = "hidden";
+  }
 
-  modal.querySelector(".modal__close-button").focus();
-}
+  /**
+   * If the form is valid, then log the form data to the console.
+   * @returns the value of the function.
+   */
+  checkValid() {
+    const isValid = (input) =>
+      input.getAttribute("data-error-visible") === "false";
 
-/**
- * If the key pressed is the escape key, then close the modal
- * @param e - The event object
- */
-function checkCloseModal(e) {
-  if (e.keyCode === KEYCODE.ESC) {
-    closeModal();
+    let inputArray = Array.from(this.modalFormData);
+
+    let formValid = inputArray.every(isValid);
+
+    if (!formValid) {
+      return false;
+    } else {
+      console.log(this.prenom, this.nom, this.email, this.message);
+    }
   }
 }
 
-
-/**
- * It removes the event listener that listens for the escape key, removes the inert attribute from all
- * the elements in the body except the modal, removes the active class from the modal, and sets the
- * overflowY property of the body to an empty string.
- */
-function closeModal() {
-  document.removeEventListener("keydown", checkCloseModal);
-
-  Array.from(document.body.children).forEach((child) => {
-    if (child !== modal) {
-      child.inert = false;
-    }
-  });
-  modal.classList.remove("active");
-  document.body.style.overflowY = "";
-
-  previousActiveElement.focus();
-}
+export { Form };
