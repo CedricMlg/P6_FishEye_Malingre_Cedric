ce9ea041c80b04d4060bef0759749e490acc9a1f
CedricMlg <malingre.cedric@gmail.com>
2022-08-04 16:54:31 +0200

    Accessibilité image lightbox

--- scripts/factories/mediaImage.js
+++ scripts/factories/mediaImage.js
@@ -7,11 +7,11 @@ class Image {
    * It takes the data from the object and creates an image element with the data from the object.
    * @returns The mediaImage variable is being returned.
    */
-  create(isTabable = false) {
-    const mediaImage = `<img ${
-      isTabable ? "" : 'tabindex="0"'
-    } src="../assets/images/${this.data.image}" alt="${this.data.title}${
-      isTabable ? "" : ", closeup view"
+  create(closeup = false) {
+    const mediaImage = `<img tabindex="0" src="../assets/images/${
+      this.data.image
+    }" alt="${this.data.title}${
+      closeup ? "" : ", closeup view"
     }" class="photographer-work__preview">`;
     return mediaImage;
   }
